bd82833802c56eb0888da23e8620f777bfadcbce
Ibrahim Azan <ibrahimazan@gmail.com>
2021-04-01 20:33:09 -0400

    Initial frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Home from './Home.js';
+import NotFound from './NotFound.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App App-header">
+        <nav >
+          EASY URL
+        </nav>
+        <Switch>
+          <Route path="/404">
+            <NotFound />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
